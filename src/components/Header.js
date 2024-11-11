d8bbf01a542742c688a2d2a4b788562eb4041a18
arkstudiodevelopers <nicksonogm@gmail.com>
2024-11-11 08:23:40 +0300

    still working on navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='App-Header'>
+        <h2 className='App-Name'>Stop Watch</h2>
+        <ul className='App-Header-List'>
+            <li>Github</li>
+            <li>Portfolio</li>
+        </ul>
+    </div>
   )
 }
 
