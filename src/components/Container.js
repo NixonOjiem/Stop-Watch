920807c8318fcf19deb7470a43cac7a2fd9748b9
arkstudiodevelopers <nicksonogm@gmail.com>
2024-11-12 07:52:50 +0300

    created a container that will house the timer, stopwatch, and click.

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Container extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Container</h1>
+      </div>
+    )
+  }
+}
+
+export default Container
