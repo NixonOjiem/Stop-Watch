23f408a34e4cd0fe7bbd172566e9672a666f6eb8
arkstudiodevelopers <nicksonogm@gmail.com>
2024-11-12 07:27:41 +0300

    still added animation to the navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header() {
     <div className='App-Header'>
         <h2 className='App-Name'>Stop Watch</h2>
         <ul className='App-Header-List'>
-            <li>Github</li>
-            <li>Portfolio</li>
+            <li><a href='https://github.com/NixonOjiem/Stop-Watch'>Github</a></li>
+            <li><a href='https://myportfolio-cde82.web.app/'>Portfolio</a></li>
         </ul>
     </div>
   )
