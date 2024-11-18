2f5709a4228fd183a2c22fc7971a8afc12ffc635
arkstudiodevelopers <nicksonogm@gmail.com>
2024-11-18 19:37:15 +0300

    added clock that shows time

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Clock() {
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className='Clock-Tile'>
         <div class="card">
-            
+            <h4>Current Time</h4> <br />
+            <h1>{time.toLocaleTimeString()}</h1>
+        </div>
+      
     </div>
-   
-   
-  </div>
 
   )
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -5,7 +5,7 @@ export class Container extends Component {
   render() {
     return (
       <div>
-        <h1>Container</h1>
+        
         <Clock />
       </div>
     )
